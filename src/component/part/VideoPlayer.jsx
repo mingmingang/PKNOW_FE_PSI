3dc49bf548581e8cb6076ba1f8f619fd04c1257b
mingmingang <saharromansah@gmail.com>
2025-09-18 07:24:52 +0700

    push 07.24 aang

--- src/component/part/VideoPlayer.jsx
+++ src/component/part/VideoPlayer.jsx
@@ -12,39 +12,6 @@ export default function KMS_VideoViewer({ videoFileName }) {
   const [isError, setIsError] = useState(false);
   const [videoUrl, setVideoUrl] = useState(null);
 
-  // useEffect(() => {
-  //   if (AppContext_test.urlMateri) {
-  //     setIsLoading(true);
-
-  //     const fetchData = async () => {
-  //       try {
-  //         const response = await axios.get(`${API_LINK}Upload/GetFile`, {
-  //           params: {
-  //             namaFile: AppContext_test.urlMateri,
-  //           },
-  //           responseType: "arraybuffer",
-  //         });
-
-  //         const blob = new Blob([response.data], {
-  //           type: response.headers["content-type"],
-  //         });
-  //         const url = URL.createObjectURL(blob);
-  //         setVideoUrl(url);
-  //         setIsError(false);
-  //         setIsLoading(false);
-  //       } catch (error) {
-  //         setIsError(true);
-  //         setIsLoading(false);
-  //       }
-  //     };
-
-  //     fetchData();
-  //   } else {
-  //     setIsError(true);
-  //     setIsLoading(false);
-  //   }
-  // }, [videoFileName, AppContext_test.urlMateri, isError]);
-
   useEffect(() => {
   if (AppContext_test.urlMateri) {
     const fetchVideoFile = async () => {
